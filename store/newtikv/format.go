304a2d5798a0fedd1e89d01d780fb0308077f87f
huangnauh <libo.huang@upai.com>
2020-08-21 11:48:18 +0800

    Feature/slow request

---
+++ store/newtikv/format.go
@@ -0,0 +1,95 @@
+package newtikv
+
+import (
+	"fmt"
+	"github.com/pingcap/tidb/util/execdetails"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func execDetailsString(execDetails *execdetails.StmtExecDetails) string {
+	if execDetails == nil {
+		return ""
+	}
+
+	var builder strings.Builder
+	waitKV := time.Duration(execDetails.WaitKVRespDuration)
+	if waitKV > slowTime {
+		builder.WriteString("Wait_kv_time: ")
+		builder.WriteString(waitKV.String())
+		builder.WriteString(" ")
+	}
+
+	waitPD := time.Duration(execDetails.WaitPDRespDuration)
+	if waitPD > slowTime {
+		builder.WriteString("Wait_pd_time: ")
+		builder.WriteString(waitPD.String())
+		builder.WriteString(" ")
+	}
+
+	backOff := time.Duration(execDetails.BackoffDuration)
+	if backOff > slowTime {
+		builder.WriteString("Back_off_time: ")
+		builder.WriteString(backOff.String())
+		builder.WriteString(" ")
+	}
+	return builder.String()
+}
+
+func commitDetailsString(commitDetails *execdetails.CommitDetails) string {
+	if commitDetails == nil {
+		return ""
+	}
+	var builder strings.Builder
+	if commitDetails.PrewriteTime > slowTime {
+		builder.WriteString("Prewrite_time: ")
+		builder.WriteString(commitDetails.PrewriteTime.String())
+		builder.WriteString(" ")
+	}
+	if commitDetails.CommitTime > slowTime {
+		builder.WriteString("Commit_time: ")
+		builder.WriteString(commitDetails.CommitTime.String())
+		builder.WriteString(" ")
+	}
+	if commitDetails.GetCommitTsTime > slowTime {
+		builder.WriteString("Get_commit_ts_time: ")
+		builder.WriteString(commitDetails.GetCommitTsTime.String())
+		builder.WriteString(" ")
+	}
+
+	commitBackOffTime := time.Duration(commitDetails.CommitBackoffTime)
+	if commitBackOffTime > slowTime {
+		builder.WriteString("Commit_back_off_time: ")
+		builder.WriteString(commitBackOffTime.String())
+		builder.WriteString(" ")
+	}
+
+	commitDetails.Mu.Lock()
+	if len(commitDetails.Mu.BackoffTypes) > 0 {
+		builder.WriteString("Back_off_types: ")
+		builder.WriteString(fmt.Sprintf("%v", commitDetails.Mu.BackoffTypes))
+		builder.WriteString(" ")
+	}
+	commitDetails.Mu.Unlock()
+
+	resolveLockTime := time.Duration(commitDetails.ResolveLockTime)
+	if resolveLockTime > slowTime {
+		builder.WriteString("Resolve_lock_time: ")
+		builder.WriteString(resolveLockTime.String())
+		builder.WriteString(" ")
+	}
+
+	if commitDetails.LocalLatchTime > slowTime {
+		builder.WriteString("Local_latch_wait_time: ")
+		builder.WriteString(commitDetails.LocalLatchTime.String())
+		builder.WriteString(" ")
+	}
+
+	if commitDetails.TxnRetry > 0 {
+		builder.WriteString("Txn_retry: ")
+		builder.WriteString(strconv.FormatInt(int64(commitDetails.TxnRetry), 10))
+		builder.WriteString(" ")
+	}
+	return builder.String()
+}
--- store/newtikv/metrics.go
+++ store/newtikv/metrics.go
@@ -2,10 +2,136 @@ package newtikv
 
 import (
 	"github.com/pingcap/tidb/metrics"
+	"github.com/pingcap/tidb/util/execdetails"
 	"github.com/prometheus/client_golang/prometheus"
+	"time"
 )
 
+type Metric struct {
+	WaitKVRespDurationHistogram *prometheus.HistogramVec
+	WaitPDRespDurationHistogram *prometheus.HistogramVec
+	BackOffDurationHistogram    *prometheus.HistogramVec
+	PrewriteTimeHistogram       *prometheus.HistogramVec
+	CommitTimeHistogram         *prometheus.HistogramVec
+	GetCommitTsTimeHistogram    *prometheus.HistogramVec
+	CommitBackOffTimeHistogram  *prometheus.HistogramVec
+	ResolveLockTimeHistogram    *prometheus.HistogramVec
+}
+
+var metric = newMetric()
+
+func (m *Metric) Observe(method string, execDetails *execdetails.StmtExecDetails,
+	commitDetails *execdetails.CommitDetails) {
+	if execDetails != nil {
+		m.WaitKVRespDurationHistogram.WithLabelValues(method).Observe(
+			time.Duration(execDetails.WaitKVRespDuration).Seconds())
+		m.WaitPDRespDurationHistogram.WithLabelValues(method).Observe(
+			time.Duration(execDetails.WaitPDRespDuration).Seconds())
+		m.BackOffDurationHistogram.WithLabelValues(method).Observe(
+			time.Duration(execDetails.BackoffDuration).Seconds())
+	}
+
+	if commitDetails != nil {
+		m.PrewriteTimeHistogram.WithLabelValues(method).Observe(
+			commitDetails.PrewriteTime.Seconds())
+		m.CommitTimeHistogram.WithLabelValues(method).Observe(
+			commitDetails.CommitTime.Seconds())
+		m.GetCommitTsTimeHistogram.WithLabelValues(method).Observe(
+			commitDetails.GetCommitTsTime.Seconds())
+		m.CommitBackOffTimeHistogram.WithLabelValues(method).Observe(
+			time.Duration(commitDetails.CommitBackoffTime).Seconds())
+		m.ResolveLockTimeHistogram.WithLabelValues(method).Observe(
+			time.Duration(commitDetails.ResolveLockTime).Seconds())
+	}
+}
+
+func newMetric() *Metric {
+	return &Metric{
+		WaitKVRespDurationHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "wait_kv_resp_duration",
+				Help:      "Bucketed histogram of tikv wait kv resp duration.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+		WaitPDRespDurationHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "wait_pd_resp_duration",
+				Help:      "Bucketed histogram of tikv wait pd resp duration.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+		BackOffDurationHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "backoff_duration",
+				Help:      "Bucketed histogram of tikv backoff duration.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+		PrewriteTimeHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "prewrite_time",
+				Help:      "Bucketed histogram of prewrite time.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+		CommitTimeHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "commit_time",
+				Help:      "Bucketed histogram of commit time.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+		GetCommitTsTimeHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "get_commit_ts_time",
+				Help:      "Bucketed histogram of get commit ts time.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+		ResolveLockTimeHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "reslove_lock_time",
+				Help:      "Bucketed histogram of reslove lock time.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+		CommitBackOffTimeHistogram: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Subsystem: "tikv",
+				Name:      "commit_backoff_time",
+				Help:      "Bucketed histogram of commit backoff time.",
+				Buckets:   prometheus.ExponentialBuckets(0.0001, 2, 21), // 0.1ms ~ 104s
+			},
+			[]string{"method"},
+		),
+	}
+}
+
 func registerMetrics() {
+	prometheus.MustRegister(metric.WaitKVRespDurationHistogram)
+	prometheus.MustRegister(metric.WaitPDRespDurationHistogram)
+	prometheus.MustRegister(metric.BackOffDurationHistogram)
+	prometheus.MustRegister(metric.PrewriteTimeHistogram)
+	prometheus.MustRegister(metric.CommitTimeHistogram)
+	prometheus.MustRegister(metric.GetCommitTsTimeHistogram)
+	prometheus.MustRegister(metric.ResolveLockTimeHistogram)
+	prometheus.MustRegister(metric.CommitBackOffTimeHistogram)
+
 	prometheus.MustRegister(metrics.TiKVBackoffHistogram)
 	prometheus.MustRegister(metrics.TiKVCoprocessorHistogram)
 	prometheus.MustRegister(metrics.TiKVLoadSafepointCounter)
